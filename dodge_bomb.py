397bd4293e95a2642d88df431cf41aff6613abef
c0b23141 <c0b23141d1@edu.teu.ac.jp>
2024-10-01 14:30:30 +0900

    練習2 :爆弾作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect() 
     kk_rct.center = 300, 200
+    bd_img = pg.Surface((20,20))
+    bd_img.set_colorkey((0,0,0))  #四隅の四角を取り除く
+    pg.draw.circle(bd_img,(255,0,0),(10,10),10)
+    bd_rct = bd_img.get_rect()  #爆弾rectの抽出
+    bd_rct.centerx = random.randint(0,WIDTH)
+    bd_rct.centery = random.randint(0,HEIGHT)
+    vx,vy = +5,+5  #爆弾の速度
+    #bd_img.set_colorkey((0,0,0))
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,6 +55,8 @@ def main():
                 sum_mv[1] += tpl[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)  # kk_retに基づいた場所に
+        bd_rct.move_ip(vx,vy)
+        screen.blit(bd_img, bd_rct)  # kk_retに基づいた場所に
         pg.display.update()
         tmr += 1
         clock.tick(50)
